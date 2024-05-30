fd9b9817cc9f29e4c89773029eb3705a89591fbc
xiaoqs2024 <3291652485@qq.com>
2024-05-30 10:07:52 +0800

    iNeckStartAndEndPart

---
+++ iNeckStartAndEndPart/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject StartView;
+    public GameObject MusicView;
+    public GameObject styleView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartButton()
+    {
+        StartView.SetActive(false);
+        styleView.SetActive(true);
+    }
+
+    public void SetUpButton()
+    {
+        StartView.SetActive(false);
+        MusicView.SetActive(true);
+    }
+    public void ReturnButton()
+    {
+        StartView.SetActive(true);
+        styleView.SetActive(false);
+        MusicView.SetActive(false);
+    }
+
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void OnePlayer()
+    {
+        SceneManager.LoadScene("OnePlayerScene");
+    }
+    public void TwoPlayer()
+    {
+        SceneManager.LoadScene("TwoPlayerScene");
+    }
+}
+
